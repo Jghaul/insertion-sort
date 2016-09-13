544bea011277bfd7b7d8a724ae0b522019bd38ab
Jghaul <Jaghaul@ursinus.edu>
2016-09-13 02:13:56 -0400

    second shot- working insertion for words and numbers

--- main.cpp
+++ main.cpp
@@ -3,79 +3,84 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <ctime>
+
 
 using namespace std;
+template <class Etype>
+
+void method(vector <Etype> & A, int wordCount){
+
+for(int i =1;i<wordCount-1;i++)
+{
+
+Etype temp = A[i];
+
+for(int j = i;j>0 && temp <A[j-1];j--)
+    {
+
+        if(A[j]<=A[j-1])
+        {
+             A[j]=A[j-1];
+             A[j-1]=temp;
+        }
+    }
+}
+}
 
 int main()
 {
 
-    int numbers;
-
-    int numberCount = 0;
+    string words;
+    int wordCount = 0;
     cout<<"Enter the filename for the file you wish to be sorted."<<endl;
 
     string input;
     cin>>input;
     ifstream textFile(input.c_str());
-    vector <int> numbersVector;
+    vector <string> A;
 
-while(textFile >> numbers){
+while(textFile >> words){
 
-
-numbersVector.push_back(numbers);
-
-
-
-
-    numberCount++;
+A.push_back(words);
+wordCount++;
 
 }
 
+clock_t begin = clock();
+
+method(A,wordCount);
+
+clock_t end = clock();
+double elapsed_secs = double(end - begin)/ CLOCKS_PER_SEC;
+cout<<elapsed_secs<<endl;
 
 
-for(int i =1;i<numberCount-1;i++)
-{
 
- int temp = numbersVector[i];
-
-
-    for(int j = i;j>0 && temp < numbersVector[j-1];j--)
+ofstream output;
+ output.open("output.txt");
+ for(int i=0;i<wordCount-1;i++){
     {
-
-        if(numbersVector[j]<=numbersVector[j-1])
-        {
-             numbersVector[j]=numbersVector[j-1];
-             numbersVector[j-1]=temp;
-        }
-
-
-
-
-
-
-
-
-
-
+    cout<<A[i]<<endl;
+    output << A[i] <<endl;
 
     }
-
-
+    output.close();
 
 
 }
- ofstream output;
- output.open("output.txt");
-for(int i=0;i<numberCount;i++)
-{
-cout<<numbersVector[i];
-
-output << numbersVector[i];
-
 }
-output.close();
 
-}
+
+
+
+
+
+
+
+
+
+
 
 
 
